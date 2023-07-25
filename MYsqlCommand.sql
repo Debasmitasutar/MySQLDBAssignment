dbcc5f6029139bfbaab17299a9e4e0795486cb8d
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-25 17:28:14 +0530

    Added  MYsqlCommand.sql

--- MYsqlCommand.sql
+++ MYsqlCommand.sql
@@ -6,3 +6,11 @@ CREATE TABLE employee_payroll(id INT Not null auto_increment,
                     salary double,
                     start DATE,
                     PRIMARY KEY(id));
+ DROP TABLE employee_payroll;
+ DESCRIBE employee_payroll;
+ INSERT INTO employee_payroll (name,salary,start) VALUES
+             ('Dipti',70000.00,'2023-04-19'),
+             ('Sarika',50000.00,'2023-02-16'),
+             ('Deba',30000.00,'2023-02-13');
+             
+             
