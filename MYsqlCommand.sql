86054b26e8f373dc69b8254ef174e4d396b51e21
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-25 18:12:15 +0530

    Added  MYsqlCommand.sql

--- MYsqlCommand.sql
+++ MYsqlCommand.sql
@@ -13,5 +13,15 @@ CREATE TABLE employee_payroll(id INT Not null auto_increment,
              ('Sarika',50000.00,'2023-02-16'),
              ('Deba',30000.00,'2023-02-13');
 SELECT * FROM employee_payroll;
+SELECT salary FROM employee_payroll WHERE name='Dipti';
+DELETE FROM employee_payroll WHERE name='Dipti';
+INSERT INTO employee_payroll (name,salary,start) VALUES 
+                   ('Dipti',70000.00,'2023-04-19');
+SELECT * FROM employee_payroll
+  WHERE start BETWEEN cast('2018-01-18' AS DATE) AND DATE (NOW());
+  
+                 
+            
+               
              
              
